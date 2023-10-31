68363839be289594fb1a1e82eb14f61d407b12a4
AvalonC <57788292+AvalonC@users.noreply.github.com>
2023-10-31 15:47:06 +0000

    Deploying to gh-pages from @ AvalonC/BIS_Navigator_V2@8910e7a376b4cd572f8992d4c5172c1d4d3bda97 🚀

---
+++ assets/BIS_resource.html-b1629977.js
@@ -0,0 +1,19 @@
+import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{r as l,o as r,c as a,b as n,d as e,a as s}from"./app-d680603e.js";const c={},_=n("p",null,"此为BIS常用资源站",-1),d=n("h2",{id:"使用规约",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#使用规约","aria-hidden":"true"},"#"),e(" 使用规约")],-1),h=n("p",null,[n("strong",null,"网盘内资料仅供BIS内部使用，严禁外传、商用、非法传播，一经发现将追究相关法律责任。")],-1),i=n("thead",null,[n("tr",null,[n("th",null,"地址"),n("th",null,"密码")])],-1),u={href:"https://pan.baidu.com/s/1kecaegEXynazWMu2I0N4fA",target:"_blank",rel:"noopener noreferrer"},f=n("td",null,"hxk2",-1),p=n("h2",{id:"网盘结构",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#网盘结构","aria-hidden":"true"},"#"),e(" 网盘结构")],-1),m=n("pre",null,[n("code",null,`.
+|- 实用文件
+    |- 粤语
+        |-粤语课录屏
+    |- 求职
+        |-内陆
+        |-留港
+        |-就业讲座
+        |-笔面资料
+    |- 教材
+    |- 攻略
+|— BIS
+    |-时间
+        |-22 Fall
+        |-21 Fall及之前
+    |-方向
+        |-MIS
+        |-FIT
+`)],-1);function I(x,B){const t=l("ExternalLinkIcon");return r(),a("div",null,[_,d,h,n("table",null,[i,n("tbody",null,[n("tr",null,[n("td",null,[n("a",u,[e("百度网盘"),s(t)])]),f])])]),p,m])}const S=o(c,[["render",I],["__file","BIS_resource.html.vue"]]);export{S as default};
